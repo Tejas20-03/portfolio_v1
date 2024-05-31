ff7a5fc69d39cc313b2a095b1df6663680800913
Tejas20-03 <tejaschhabra.official@gmail.com>
2024-05-31 16:37:05 +0530

    Added Navbar,Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,119 @@
-import React from 'react'
+import React from "react";
+import anima from "../assets/anima.json";
+import Lottie from "lottie-react";
+import link from "../assets/link.png";
+import github from "../assets/git.png";
+import x from "../assets/x.png";
+import insta from "../assets/insta.svg";
+import { Fade } from "react-awesome-reveal";
+import { Link } from "react-scroll";
+import { Context } from "./Context";
 
 const Home = () => {
+  const { theme } = React.useContext(Context);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div
+        className={`mx-auto h-screen px-20 sm:px-10 transition-all duration-700 ease-in-out ${
+          theme ? "invert" : "invert-0"
+        } `}
+      >
+        <div className="flex gap-5  justify-end mt-2 sm:justify-center">
+          <Fade cascade damping={0.2} triggerOnce delay={800} direction="down">
+            <a
+              href="
+              https://www.linkedin.com/in/tejas20/"
+              target="_blank"
+            >
+              <img
+                src={link}
+                alt="link"
+                className="sm:w-7 w-9 aspect-square hover:-translate-y-1 transition-all duration-300 ease-in-out"
+              />
+            </a>
+            <a
+              href="
+            https://www.instagram.com/teeee_seee/"
+              target="_blank"
+            >
+              <img
+                src={insta}
+                alt="insta"
+                className="sm:w-7 w-9 aspect-square hover:-translate-y-1 transition-all duration-300 ease-in-out"
+              />
+            </a>
+            <a
+              href="
 
-export default Home
+              https://www.github.com/Tejas20-03"
+              target="_blank"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="sm:w-7 w-9 aspect-square hover:-translate-y-1 transition-all duration-300 ease-in-out"
+              />
+            </a>
+
+            <a href="https://www.twitter.com/peproni20" target="_blank">
+              <img
+                src={x}
+                alt="x"
+                className="sm:w-7 w-9 aspect-square hover:-translate-y-1 transition-all duration-300 ease-in-out"
+              />
+            </a>
+          </Fade>
+        </div>
+        <div className="flex justify-between ">
+          <div className="flex sm:flex-col md:flex-col md:mt-10 ">
+            <div className="w-1/2 h-full md:w-full flex justify-center align-middle my-auto flex-col gap-6 sm:mt-10">
+              <Fade triggerOnce direction="down">
+                <h1 className="text-8xl font-extrabold sm:text-7xl">
+                  Hey there,
+                </h1>
+              </Fade>
+              <Fade triggerOnce delay={200}>
+                <h1 className="text-xl leading-loose sm:leading-relaxed ">
+                  I'm{" "}
+                  <span className="font-semibold text-3xl sm:text-2xl ">
+                    Tejas Chhabra
+                  </span>{" "}
+                  , your friendly neighborhood <br />
+                  <span className="font-semibold text-2xl">
+                    Full-Stack Web Developer
+                  </span>
+                  . I code with passion and transform ideas into seamless
+                  digital experiences.{" "}
+                </h1>
+
+                <p className="text-xl">
+                  {" "}
+                  Let's build something amazing together!
+                </p>
+              </Fade>
+
+              <Fade triggerOnce direction="up" className="mx-auto" delay={300}>
+                <Link to="contact" smooth={true} duration={1000} offset={-10}>
+                  <button className="px-10 py-4 text-xl mt-10 sm:mt-10 cursor-pointer transition-all duration-300 ease-in-out hover:-translate-y-2 mx-auto border-2 rounded-lg border-black font-semibold w-fit">
+                    Let's Chat
+                  </button>
+                </Link>
+              </Fade>
+            </div>
+
+            <div className="flex-1 sm:hidden ">
+              <Fade direction="up" triggerOnce delay={300}>
+                <Lottie
+                  animationData={anima}
+                  className="md:w-[75%] mx-auto invert"
+                />
+              </Fade>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React from "react";
+import home from "../assets/home.png";
+import about from "../assets/about.svg";
+import contact from "../assets/contact2.png";
+import skill from "../assets/skills2.png";
+import projects from "../assets/projects3.png";
+import { Tooltip } from "antd";
+import { useState, useEffect } from "react";
+import { Context } from "./Context";
+
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const { theme } = React.useContext(Context);
 
-export default Navbar
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 50);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  return (
+    <div
+      className={`flex ${
+        theme ? "invert" : "invert-0"
+      } justify-center fixed bottom-16 left-0 right-0 z-50 ${
+        visible ? "translate-y-0" : "translate-y-56 "
+      } transition-all duration-700 ease-in-out}`}
+    >
+      <ul className="flex gap-8 sm:gap-5 bg-white w-fit border-2 border-black px-7 py-2 rounded-lg  justify-center drop-shadow-2xl sm:p-3">
+        <li className="group cursor-pointer">
+          <Tooltip title="Home" color={`${theme ? "gray" : "black"}`}>
+            <Link to="home" smooth={true} duration={1000} offset={-10}>
+              <img
+                src={home}
+                alt="home"
+                className={`w-12  sm:w-10 aspect-square group-hover:-translate-y-2 transition-all duration-300 ease-in-out hover:drop-shadow-2xl`}
+              />
+            </Link>
+          </Tooltip>
+        </li>
+        <li className="group cursor-pointer">
+          <Tooltip title="About" color={`${theme ? "gray" : "black"}`}>
+            <Link to="about" smooth={true} duration={1000} offset={-40}>
+              <img
+                src={about}
+                alt="about"
+                className="w-12  sm:w-10 aspect-square group-hover:-translate-y-2 transition-all duration-300 ease-in-out hover:drop-shadow-2xl"
+              />
+            </Link>
+          </Tooltip>
+        </li>
+        <li className="group cursor-pointer">
+          <Tooltip title="Skills" color={`${theme ? "gray" : "black"}`}>
+            <Link to="skills" smooth={true} duration={1000} offset={-200}>
+              <img
+                src={skill}
+                alt="skills"
+                className="w-12  sm:w-10 aspect-square group-hover:-translate-y-2 transition-all duration-300 ease-in-out hover:drop-shadow-2xl"
+              />
+            </Link>
+          </Tooltip>
+        </li>
+        <li className="group cursor-pointer">
+          <Tooltip title="Projects" color={`${theme ? "gray" : "black"}`}>
+            <Link to="projects" smooth={true} duration={1000} offset={40}>
+              <img
+                src={projects}
+                alt="projects"
+                className="w-12  sm:w-10 aspect-square group-hover:-translate-y-2 transition-all duration-300 ease-in-out hover:drop-shadow-2xl"
+              />
+            </Link>
+          </Tooltip>
+        </li>
+        <li className="group cursor-pointer">
+          <Tooltip title="Contact Me" color={`${theme ? "gray" : "black"}`}>
+            <Link to="contact" smooth={true} duration={1000}>
+              <img
+                src={contact}
+                alt="contactme"
+                className="w-12  sm:w-10 aspect-square group-hover:-translate-y-2 transition-all duration-300 ease-in-out hover:drop-shadow-2xl"
+              />
+            </Link>
+          </Tooltip>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
